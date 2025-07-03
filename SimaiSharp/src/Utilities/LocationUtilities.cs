9ccfc995329779f15c0528317c856cd7dfd3d45f
fumiko <2394425147@qq.com>
2025-07-03 13:56:37 +0800

    feat: Implement location group and location index modification extensions

--- SimaiSharp/src/Utilities/LocationUtilities.cs
+++ SimaiSharp/src/Utilities/LocationUtilities.cs
@@ -13,6 +13,14 @@ namespace SimaiSharp.Utilities
             (location & 0xF0) >> 4;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int WithNoteGroup(this int location, int group) =>
+            (location & 0x0F) | (group << 4);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int WithNoteIndex(this int location, int index) =>
+            (location & 0xF0) | index;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsTap(this int location) =>
             location.ToNoteGroup() == 0;
 
