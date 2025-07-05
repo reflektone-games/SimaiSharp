4c98949d68ff978659c0f475f8ee180397d80237
fumiko <2394425147@qq.com>
2025-07-05 23:39:55 +0800

    bugfix: Wrap note rotation at 7 instead of F

--- SimaiSharp/src/Utilities/LocationUtilities.cs
+++ SimaiSharp/src/Utilities/LocationUtilities.cs
@@ -29,9 +29,7 @@ namespace SimaiSharp.Utilities
             location.ToNoteGroup() != 0;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int RotateNoteIndex(this int location, int delta)
-        {
-            return location.ToNoteGroup() == 0xC ? location : location.WithNoteIndex((location.ToNoteIndex() + delta) & 0x0F);
-        }
+        public static int RotateNoteIndex(this int location, int delta) =>
+            location.ToNoteGroup() == 0xC ? location : location.WithNoteIndex((location.ToNoteIndex() + delta) & 7);
     }
 }
